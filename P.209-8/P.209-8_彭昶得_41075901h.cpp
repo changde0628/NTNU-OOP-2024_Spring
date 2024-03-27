e820eb373348bd151271887ca7d7000b6b9edfa9
cdp <changde0628@icloud.com>
2024-03-27 14:17:49 +0800

    fix P.209-8 description error

--- P.209-8/P.209-8_彭昶得_41075901h.cpp
+++ P.209-8/P.209-8_彭昶得_41075901h.cpp
@@ -4,9 +4,11 @@ Author : chang de (41075901h@gapps.ntnu.edu.tw)
 FileName : P.209-8_彭昶得_41075901h.cpp
 -------------------------------------------------------------------------------
 Description of the problem :
-The following flowchart contains a series of questions to determine what kind
-of haircut to get. Write a program that asks the questions to the user and
-outputs the recommended haircut.
+An approximate value of pi can be calculated using the series given below:
+pi = 4 [ 1 - 1/3 + 1/5 - 1/7 + 1/9... + ( (-1)^n ) / (2n + 1) ]
+Write a C++ program to calculate the approximate value of pi using this series. The program takes an input n that
+determines the number of terms in the approximation of the value of pi and outputs the approximation. Include a loop
+that allows the user to repeat this calculation for new values n until the user says she or he wants to end the program.
 ================================================================================
 */
 
